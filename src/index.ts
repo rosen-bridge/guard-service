2ed85844a2c214ee5e4e294298d6683220645e1d
Moein zargarzadeh <moein.zargarzadeh@gmail.com>
2022-06-08 15:01:29 +0430

    split api router

--- src/index.ts
+++ src/index.ts
@@ -1,80 +1,17 @@
-import express from "express";
-import { Request, Response } from 'express';
-import { PaymentTransaction, EventTrigger } from "./models/Models";
-import { EventTriggerModel, PaymentTransactionJsonModel } from "./models/Interfaces";
-import EventProcessor from "./guard/EventProcessor";
-
-const eventProcessor = new EventProcessor()
+import express, {Router} from "express";
+import {p2pRouter} from "./api/p2p";
+import {paymentRouter} from "./api/payment";
 
 const app = express();
 const port = 8080;
 
 app.use(express.json())
+const router = Router();
 
-/**
- * Api for generating payment transaction for an event
- */
-app.post("/generate/", async (req: Request, res: Response) => {
-    const eventJson: EventTriggerModel = req.body
-    const event = new EventTrigger(
-        eventJson.fromChain,
-        eventJson.toChain,
-        eventJson.fromAddress,
-        eventJson.toAddress,
-        eventJson.amount,
-        eventJson.bridgeFee,
-        eventJson.networkFee,
-        eventJson.sourceChainTokenId,
-        eventJson.targetChainTokenId,
-        eventJson.sourceTxId,
-        eventJson.sourceBlockId,
-        eventJson.WIDs
-    )
-    const paymentTx = await eventProcessor.createEventPayment(event)
+router.use('/payment', paymentRouter)
+router.use('/p2p', p2pRouter)
 
-    res.send({
-        txId: paymentTx.txId,
-        eventId: paymentTx.eventId,
-        txBytes: paymentTx.getTxHexString()
-    })
-});
-
-/**
- * Api for verifying payment transaction with an event
- */
-app.post("/verify/", async (req: Request, res: Response) => {
-    const eventJson: EventTriggerModel = req.body.event
-    const paymentTxJson: PaymentTransactionJsonModel = req.body.paymentTx
-    const event = new EventTrigger(
-        eventJson.fromChain,
-        eventJson.toChain,
-        eventJson.fromAddress,
-        eventJson.toAddress,
-        eventJson.amount,
-        eventJson.bridgeFee,
-        eventJson.networkFee,
-        eventJson.sourceChainTokenId,
-        eventJson.targetChainTokenId,
-        eventJson.sourceTxId,
-        eventJson.sourceBlockId,
-        eventJson.WIDs
-    )
-    const paymentTx = new PaymentTransaction(
-        paymentTxJson.txId,
-        paymentTxJson.eventId,
-        Buffer.from(paymentTxJson.txBytes, "hex")
-    )
-
-    const isValid = eventProcessor.verifyPaymentTransactionWithEvent(paymentTx, event)
-
-    res.send({
-        txId: paymentTx.txId,
-        eventId: paymentTx.eventId,
-        txBytes: paymentTx.getTxHexString(),
-        isValid
-    })
-});
-
+app.use(router)
 app.listen(port, () => {
     console.log(`server started at http://localhost:${port}`);
 });
