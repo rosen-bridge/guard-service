fd48f91bd98660cf5d46bfe31a05567a25c631c1
Moein zargarzadeh <moein.zargarzadeh@gmail.com>
2022-06-08 15:02:54 +0430

    Add p2p communication

---
+++ src/api/p2p.ts
@@ -0,0 +1,44 @@
+import {Request, Response, Router} from "express";
+import {apiCallBack} from "../communication/CallbackUtils";
+import Dialer from "../communication/Dialer";
+import assert from "assert";
+
+export const p2pRouter = Router();
+const dialer = await Dialer.getInstance()
+
+
+/**
+ * Api for send a message over p2p protocol
+ * @bodyParam {string}
+ * @bodyParam {object}
+ * @bodyParam {string}
+ */
+p2pRouter.post("/send", async (req: Request, res: Response) => {
+    try {
+        assert(req.body.channel, "key channel is required!")
+        assert(req.body.message, "key message is required!")
+        req.body.receiver ?
+            dialer.sendMessage(req.body.channel, req.body.message, req.body.receiver):
+            dialer.sendMessage(req.body.channel, req.body.message)
+
+        res.send({message: "ok"})
+    }
+    catch (error) {
+        res.status(400).send({message: error.message})
+    }
+});
+
+/**
+ * Api for send a message over p2p protocol
+ */
+p2pRouter.post("/channel/subscribe", async (req: Request, res: Response) => {
+    try {
+        assert(req.body.channel, "key channel is required!")
+        assert(req.body.url, "key url is required!")
+        dialer.subscribeChannel(req.body.channel, apiCallBack, req.body.url)
+        res.send({message: "ok"})
+    }
+    catch (error) {
+        res.status(400).send({message: error.message})
+    }
+});
