6f3fd68e717f72b55c2925702ace7b9bd26d9cfb
HFazelinia <hadifazelinia78@gmail.com>
2022-06-22 15:37:50 +0000

    improve project structure

--- config/signOrmConfig.ts
+++ config/signOrmConfig.ts
@@ -8,8 +8,8 @@ const __dirname = path.dirname(__filename);
 export const signOrmConfig = new DataSource({
     type: "sqlite",
     database: __dirname + "/../sqlite/sign.sqlite",
-    entities: ['src/entities/sign/*.ts'],
-    migrations: ['src/migrations/sign/*.ts'],
+    entities: ['src/db/entities/sign/*.ts'],
+    migrations: ['src/db/migrations/sign/*.ts'],
     synchronize: false,
     logging: false
 });
