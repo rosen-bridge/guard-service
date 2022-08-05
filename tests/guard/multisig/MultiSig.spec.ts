f2b122fa22ec7b713d4b1ae95961698670237c51
sahandzou <sahandzoufan79@gmail.com>
2022-08-05 15:40:52 +0430

    adding multisig doc string

--- tests/guard/multisig/MultiSig.spec.ts
+++ tests/guard/multisig/MultiSig.spec.ts
@@ -17,6 +17,16 @@ const publicKeys = [
 
 describe("MultiSigHandler", () => {
     describe("sendMessage", () => {
+        /**
+         * Target: testing that sendMessage send message with specific keys
+         *
+         * Dependencies:
+         *  -
+         *
+         *  Expected:
+         *      the sendMessage should send json string that have "sign" & "payload" key in the main body
+         *          and have "index", "id", "nonce" and "myId" in the payload key object.
+         */
         it("checks that correct message is sent over p2p or not", () => {
             const handler = new MultiSigHandler(publicKeys, "5bc1d17d0612e696a9138ab8e85ca2a02d0171440ec128a9ad557c28bd5ea046")
             sendMessageBodyAndPayloadArguments(['sign'], ['index', 'id', 'nonce', 'myId'])
@@ -32,13 +42,27 @@ describe("MultiSigHandler", () => {
     })
 
     describe("getIndex", () => {
-        it("checks index should be 0", () => {
+        /**
+         * Target: testing getIndex returns correct index of the gaurde
+         * Dependencies:
+         *  -
+         *  Expected:
+         *      that getIndex should return 0
+         */
+        it("should return 0", () => {
             const handler = new MultiSigHandler(publicKeys, "5bc1d17d0612e696a9138ab8e85ca2a02d0171440ec128a9ad557c28bd5ea046")
             expect(handler.getIndex()).to.be.equal(0);
         })
     })
 
     describe("getProver", () => {
+        /**
+         * Target: testing getProver runs with no error
+         * Dependencies:
+         *  -
+         *  Expected:
+         *      getProver throw no error
+         */
         it("should return prover with no error", () => {
             const handler = new MultiSigHandler(publicKeys, "5bc1d17d0612e696a9138ab8e85ca2a02d0171440ec128a9ad557c28bd5ea046")
             const wallet = handler.getProver();
@@ -46,6 +70,14 @@ describe("MultiSigHandler", () => {
     })
 
     describe("handleApprove", () => {
+        /**
+         * Target: testing handleApprove should send json message with specific keys
+         * Dependencies:
+         *  -
+         *  Expected:
+         *      handleApprove should send json message with 'type', 'sign', 'payload' body key
+         *      'nonceToSign' payload key
+         */
         it("checks handler approve called without error", () => {
             const handler = new MultiSigHandler(publicKeys, "5bc1d17d0612e696a9138ab8e85ca2a02d0171440ec128a9ad557c28bd5ea046")
             handler.handleApprove('sender', {index: 1, nonce: 'nonce', myId: "sender", nonceToSign: "1"});
@@ -54,6 +86,13 @@ describe("MultiSigHandler", () => {
     })
 
     describe("handleMessage", () => {
+
+        /**
+         * Target: testing handleMessage calling correct handle function
+         * Dependencies:
+         *  -
+         *  Expected: handleCommitment called and other handlers don't call
+         */
         it("checks that handleCommitment should called", () => {
             const handler = new MultiSigHandler(
                 publicKeys,
@@ -74,7 +113,14 @@ describe("MultiSigHandler", () => {
             expect(spiedApprove.called).to.be.false;
             expect(spiedRegister.called).to.be.false;
         })
-        it("checks that approve should called", () => {
+
+        /**
+         * Target: testing handleMessage calling correct handle function
+         * Dependencies:
+         *  -
+         *  Expected: handleCommitment called and other handlers don't call
+         */
+        it("checks that handleApprove should called", () => {
             const handler = new MultiSigHandler(
                 publicKeys,
                 "168e8fee8ac6965832d6c1c17cdf60c1b582b09f293d8bd88231e32740e3b24f"
@@ -94,7 +140,14 @@ describe("MultiSigHandler", () => {
             expect(spiedCommitment.called).to.be.false;
             expect(spiedRegister.called).to.be.false;
         })
-        it("checks that register should called", () => {
+
+        /**
+         * Target: testing handleMessage calling correct handle function
+         * Dependencies:
+         *  -
+         *  Expected: handleRegister called and other handlers don't call
+         */
+        it("checks that handleRegister should called", () => {
             const handler = new MultiSigHandler(
                 publicKeys,
                 "168e8fee8ac6965832d6c1c17cdf60c1b582b09f293d8bd88231e32740e3b24f"
@@ -114,7 +167,14 @@ describe("MultiSigHandler", () => {
             expect(spiedCommitment.called).to.be.false;
             expect(spiedApprove.called).to.be.false;
         })
-        it("checks that sign should called", () => {
+
+        /**
+         * Target: testing handleMessage calling correct handle function
+         * Dependencies:
+         *  -
+         *  Expected: handleSign called and other handlers don't call
+         */
+        it("checks that handleSign should called", () => {
             const handler = new MultiSigHandler(
                 publicKeys,
                 "168e8fee8ac6965832d6c1c17cdf60c1b582b09f293d8bd88231e32740e3b24f"
@@ -138,6 +198,13 @@ describe("MultiSigHandler", () => {
     })
 
     describe("getPeerId", () => {
+
+        /**
+         * Target: test that getPeerId returns valid dialer message
+         * Dependencies:
+         *  -
+         *  Expected: getPeerId return 'peerId' as peerId
+         */
         it("checks peerId should be peerId", () => {
             const handler = new MultiSigHandler(publicKeys, "5bc1d17d0612e696a9138ab8e85ca2a02d0171440ec128a9ad557c28bd5ea046")
             expect(handler.getPeerId()).to.be.eql("peerId");
@@ -145,6 +212,13 @@ describe("MultiSigHandler", () => {
     })
 
     describe("handleRegister", () => {
+
+        /**
+         * Target: test that handleRegister called sendMessage
+         * Dependencies:
+         *  -
+         *  Expected: sendMessage called once
+         */
         it("checks that handle register called without error", () => {
             const handler = new MultiSigHandler(publicKeys, "5bc1d17d0612e696a9138ab8e85ca2a02d0171440ec128a9ad557c28bd5ea046")
             const spiedSendMessage = sinon.spy(handler, "sendMessage");
@@ -154,6 +228,14 @@ describe("MultiSigHandler", () => {
     })
 
     describe("handleSign", () => {
+
+        /**
+         * Target: test that handleSign runs with no error when updateSign variable
+         *  is true
+         * Dependencies:
+         *  -
+         *  Expected: tests runs with no error
+         */
         it("checks that handleSign called with no error with updateSign set to true", () => {
             //TODO: should check that generateSign called for now it checks that functions runs without error
             const box1Hex = '80a8d6b907100304000e20a6ac381e6fa99929fd1477b3ba9499790a775e91d4c14c5aa86e9a118dfac8530400d801d601b2db6501fe730000ea02d1aedb63087201d901024d0e938c720201730198b2e4c672010510730200ade4c67201041ad901020ecdee72028cc10f00003a4f8dac9bbe80fffaf400edd5779b7ccd5628beceab06c41b5b7b3e091e963501'
@@ -172,6 +254,13 @@ describe("MultiSigHandler", () => {
             handler.handleSign('sender', {signed: ["1"], simulated: ["2"], tx: "", txId: "txid"});
         })
 
+        /**
+         * Target: test that handleSign runs with no error when updateSign variable
+         *  is false
+         * Dependencies:
+         *  -
+         *  Expected: tests runs with no error
+         */
         it("checks that handleSign called with no error with updateSign set to false", () => {
             //TODO: should check that generateSign called for now it checks that functions runs without error
             const box1Hex = '80a8d6b907100304000e20a6ac381e6fa99929fd1477b3ba9499790a775e91d4c14c5aa86e9a118dfac8530400d801d601b2db6501fe730000ea02d1aedb63087201d901024d0e938c720201730198b2e4c672010510730200ade4c67201041ad901020ecdee72028cc10f00003a4f8dac9bbe80fffaf400edd5779b7ccd5628beceab06c41b5b7b3e091e963501'
@@ -193,7 +282,15 @@ describe("MultiSigHandler", () => {
     })
 
     describe("handleCommitment", () => {
-        it("checks that handleCommitment called with no erro", () => {
+
+        /**
+         * Target: test that handleCommitment runs with no error
+         *
+         * Dependencies:
+         *  -
+         *  Expected: test runs with no error
+         */
+        it("checks that handleCommitment called with no error", () => {
             //TODO: should check that generateSign called for now it checks that functions runs without error
             const box1Hex = '80a8d6b907100304000e20a6ac381e6fa99929fd1477b3ba9499790a775e91d4c14c5aa86e9a118dfac8530400d801d601b2db6501fe730000ea02d1aedb63087201d901024d0e938c720201730198b2e4c672010510730200ade4c67201041ad901020ecdee72028cc10f00003a4f8dac9bbe80fffaf400edd5779b7ccd5628beceab06c41b5b7b3e091e963501'
             const dataBoxHex = '80ade2041006040004000400040004000402d804d601b2a5730000d602e4c6a7041ad603e4c6a70510d604ad7202d901040ecdee7204ea02d19683020193c27201c2a7938cb2db63087201730100018cb2db6308a773020001eb02ea02d19683020193e4c67201041a720293e4c672010510720398b2e4c6b2db6501fe7303000510730400720498b2720373050072048cc10f01a6ac381e6fa99929fd1477b3ba9499790a775e91d4c14c5aa86e9a118dfac85301021a0421028d938d67befbb8ab3513c44886c16c2bcd62ed4595b9b216b20ef03eb8fb8fb82103074e09c476bb215dc3aeff908d0b7691895a99dfc3bd950fa629defe541e0364210300e8750a242ee7d78f5b458e1f7474bd884d2b7894676412ba6b5f319d2ee41021023a5b48c87cd9fece23f5acd08cb464ceb9d76e3c1ddac08206980a295546bb2e100206081d827c338829135cc5c7d7f03ad9ba8ecffc6f5cddf63a2655c55922786230c000'
@@ -217,6 +314,7 @@ describe("MultiSigHandler", () => {
     })
 
     describe("cleanup", () => {
+
         /**
          * add two instance of transaction to multi-sig handler using getTransaction
          * then clean called.
