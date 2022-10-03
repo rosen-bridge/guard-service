8333780a0b92075baffa0988206d3bbba3e5e14f
sahandzou <sahandzoufan79@gmail.com>
2022-10-03 17:18:39 +0330

    adding logging level in config & loggin in stdout

--- src/log/Logger.ts
+++ src/log/Logger.ts
@@ -15,9 +15,15 @@ class Logger {
   private readonly logFormat = printf(({ level, message, timestamp }) => {
     return `${timestamp} ${level} ${message}`;
   });
-
+  private readonly logLevels = {
+    error: 0,
+    warn: 1,
+    info: 2,
+  };
+  private readonly logLevel = Configs.logLevel;
   constructor() {
     this.logger = winston.createLogger({
+      levels: this.logLevels,
       format: winston.format.combine(
         winston.format.timestamp(),
         this.logFormat
@@ -26,6 +32,11 @@ class Logger {
         new winston.transports.DailyRotateFile({
           filename: this.logsPath + 'rosen-%DATE%.log',
           ...this.logOptions,
+          level: this.logLevel,
+        }),
+        new winston.transports.Console({
+          format: winston.format.simple(),
+          level: this.logLevel,
         }),
       ],
 
@@ -34,6 +45,7 @@ class Logger {
         new winston.transports.DailyRotateFile({
           filename: this.logsPath + 'rosen-%DATE%.log',
           ...this.logOptions,
+          level: this.logLevel,
         }),
       ],
 
@@ -42,21 +54,10 @@ class Logger {
         new winston.transports.DailyRotateFile({
           filename: this.logsPath + 'rosen-%DATE%.log',
           ...this.logOptions,
+          level: this.logLevel,
         }),
       ],
     });
-
-    //in case of development environment logs should be written in console
-    if (
-      process.env.NODE_ENV === 'development' ||
-      process.env.NODE_ENV === 'test'
-    ) {
-      this.logger.add(
-        new winston.transports.Console({
-          format: winston.format.simple(),
-        })
-      );
-    }
   }
 }
 
