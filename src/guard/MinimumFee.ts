12f84c1cfd17b48245728fa1dd540929c538e477
HFazelinia <hadifazelinia78@gmail.com>
2022-10-25 13:38:15 +0000

    fix templateHash parameter in MinimumFee class

--- src/guard/MinimumFee.ts
+++ src/guard/MinimumFee.ts
@@ -1,11 +1,12 @@
 import { BridgeMinimumFee } from '@rosen-bridge/minimum-fee';
 import ErgoConfigs from '../chains/ergo/helpers/ErgoConfigs';
 import { rosenConfig } from '../helpers/RosenConfig';
+import Configs from '../helpers/Configs';
 
 class MinimumFee {
   static bridgeMinimumFee = new BridgeMinimumFee(
     ErgoConfigs.explorer.url,
-    'TODO',
+    Configs.minimumFeeConfigBoxTemplateHash,
     rosenConfig.rsnRatioNFT
   );
 }
