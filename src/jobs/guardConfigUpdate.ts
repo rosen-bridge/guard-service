ee15ee3286d3be6e797e1766aa87f8bce05a5193
fateme.r <fatemeh.r3366@gmail.com>
2022-11-04 16:57:53 +0330

    config update error handling

--- src/jobs/guardConfigUpdate.ts
+++ src/jobs/guardConfigUpdate.ts
@@ -1,15 +1,23 @@
 import { guardConfig } from '../helpers/GuardConfig';
 import Configs from '../helpers/Configs';
+import { logger } from '../log/Logger';
 
 /**
  * updates the guard config periodically
  */
-const configUpdateJob = () => {
-  guardConfig
-    .setConfig()
-    .then(() =>
-      setTimeout(configUpdateJob, Configs.guardConfigUpdateInterval * 1000)
-    );
+const configUpdateJob = async () => {
+  try {
+    await guardConfig.setConfig();
+    setTimeout(configUpdateJob, Configs.guardConfigUpdateInterval * 1000);
+  } catch (e) {
+    if (e instanceof Error) {
+      logger.warn(`Updating guard config failed with error: ${e.message}`);
+      setTimeout(configUpdateJob, Configs.guardConfigUpdateInterval * 1000);
+    } else {
+      logger.error('Guard config updating failed');
+      throw e;
+    }
+  }
 };
 
 /**
