dcf6e9082794cc882cc339822785d5389f758716
HFazelinia <hadifazelinia78@gmail.com>
2023-01-11 13:01:46 +0000

    add lastStatusUpdate column to TransactionEntity

--- src/db/migrations/index.ts
+++ src/db/migrations/index.ts
@@ -1,4 +1,9 @@
 import { migration1668494432249 } from './1668494432249-migration';
 import { migration1669739759526 } from './1669739759526-migration';
+import { migration1673441792000 } from './1673441792000-migration';
 
-export default [migration1668494432249, migration1669739759526];
+export default [
+  migration1668494432249,
+  migration1669739759526,
+  migration1673441792000,
+];
