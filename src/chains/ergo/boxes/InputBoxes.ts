ad274d3659f83f65a07cd50b347c8c138b553d8b
HFazelinia <hadifazelinia78@gmail.com>
2023-01-31 16:24:43 +0000

    fix bug and log improvement

--- src/chains/ergo/boxes/InputBoxes.ts
+++ src/chains/ergo/boxes/InputBoxes.ts
@@ -104,23 +104,21 @@ class InputBoxes {
     a: ErgoBoxCandidate,
     b: ErgoBoxCandidate
   ): number => {
-    const aR4 = a.register_value(4)?.to_coll_coll_byte()[0];
-    const bR4 = b.register_value(4)?.to_coll_coll_byte()[0];
+    if (a.ergo_tree().to_base16_bytes() < b.ergo_tree().to_base16_bytes())
+      return -1;
+    else if (a.ergo_tree().to_base16_bytes() > b.ergo_tree().to_base16_bytes())
+      return 1;
+    else {
+      const aR4 = a.register_value(4)?.to_coll_coll_byte()[0];
+      const bR4 = b.register_value(4)?.to_coll_coll_byte()[0];
 
-    if (aR4 !== undefined && bR4 !== undefined) {
-      const aWID = Buffer.from(aR4).toString('hex');
-      const bWID = Buffer.from(bR4).toString('hex');
-      if (aWID < bWID) return -1;
-      else if (aWID > bWID) return 1;
-      else return 0;
-    } else {
-      if (a.ergo_tree().to_base16_bytes() < b.ergo_tree().to_base16_bytes())
-        return -1;
-      else if (
-        a.ergo_tree().to_base16_bytes() > b.ergo_tree().to_base16_bytes()
-      )
-        return 1;
-      else return 0;
+      if (aR4 !== undefined && bR4 !== undefined) {
+        const aWID = Buffer.from(aR4).toString('hex');
+        const bWID = Buffer.from(bR4).toString('hex');
+        if (aWID < bWID) return -1;
+        else if (aWID > bWID) return 1;
+        else return 0;
+      } else return 0;
     }
   };
 }
