7f8d8f6bbaa8d7c667e56264076ba91679dfee08
Mohammad Kermani <mkermani144@gmail.com>
2023-02-22 09:26:24 +0330

    Add a debug log for json-big parsing errors
    Wrap current json-big object in another object which behaves exactly the
    same, but logs any error (and its corresponding data) that occur during
    parsing (and re-throwing the error).

--- src/network/NetworkModels.ts
+++ src/network/NetworkModels.ts
@@ -1,6 +1,26 @@
 import JSONBigInt from 'json-bigint';
 
-export const JsonBI = JSONBigInt({
+import { loggerFactory } from '../../src/log/Logger';
+
+const JsonBI = JSONBigInt({
   useNativeBigInt: true,
   alwaysParseAsBig: true,
 });
+
+const wrappedJsonBi: typeof JsonBI = {
+  stringify: JsonBI.stringify,
+  parse: (...args) => {
+    try {
+      return JsonBI.parse(...args);
+    } catch (error) {
+      const logger = loggerFactory(import.meta.url);
+      logger.debug(`An error occurred while parsing value using json-bigint.`, {
+        value: args[0],
+      });
+
+      throw error;
+    }
+  },
+};
+
+export { wrappedJsonBi as JsonBI };
