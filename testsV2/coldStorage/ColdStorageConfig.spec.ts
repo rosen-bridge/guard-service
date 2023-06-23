a46b882f608d625243e4c61f4856716429a87155
HFazelinia <hadifazelinia78@gmail.com>
2023-06-23 11:12:36 +0100

    add minor test

--- testsV2/coldStorage/ColdStorageConfig.spec.ts
+++ testsV2/coldStorage/ColdStorageConfig.spec.ts
@@ -26,5 +26,22 @@ describe('ColdStorageConfig', () => {
       const result = ColdStorageConfig.isWithinTime();
       expect(result).to.equal(true);
     });
+
+    /**
+     * @target ColdStorageConfig.isWithinTime should return false
+     * when current timestamp is NOT within hours
+     * @dependencies
+     * - Date
+     * @scenario
+     * - run test
+     * - check returned value
+     * @expected
+     * - returned value should be false
+     */
+    it('should return true when current timestamp is NOT within config hours', async () => {
+      vi.advanceTimersByTime(7200000);
+      const result = ColdStorageConfig.isWithinTime();
+      expect(result).to.equal(false);
+    });
   });
 });
