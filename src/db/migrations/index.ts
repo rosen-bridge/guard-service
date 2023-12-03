33450850313a9b9c14cb5f5bc0b94832a7b91f27
HFazelinia <hadifazelinia78@gmail.com>
2023-12-03 12:08:06 +0000

    add postgres migration

--- src/db/migrations/index.ts
+++ src/db/migrations/index.ts
@@ -1,8 +1,9 @@
 import { migration1700756107393 } from './postgres/1700756107393-migration';
+import { migration1701604819671 } from './postgres/1701604819671-migration';
 import { migration1700755909353 } from './sqlite/1700755909353-migration';
 import { migration1701494227818 } from './sqlite/1701494227818-migration';
 
 export default {
   sqlite: [migration1700755909353, migration1701494227818],
-  postgres: [migration1700756107393],
+  postgres: [migration1700756107393, migration1701604819671],
 };
