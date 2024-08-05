249feee28210d5554300f31b01c1df7a9a460ce1
HFazelinia <hadifazelinia78@gmail.com>
2024-08-05 15:50:54 +0100

    update migrations
    - two migrations to cover block_entity changes in @rosen-bridge/scanner
    - one migration to add unexpctedFails column to confirmed_event_entity

---
+++ src/db/migrations/postgres/1722866531899-migration.ts
@@ -0,0 +1,18 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class migration1722866531899 implements MigrationInterface {
+  name = 'migration1722866531899';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+            ALTER TABLE "confirmed_event_entity"
+            ADD "unexpectedFails" integer NOT NULL DEFAULT '0'
+        `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+            ALTER TABLE "confirmed_event_entity" DROP COLUMN "unexpectedFails"
+        `);
+  }
+}
