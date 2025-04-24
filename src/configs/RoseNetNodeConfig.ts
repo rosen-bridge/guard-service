9b7e143ce4140d7f36ec476ef97dec308cfec10c
Moein Zargarzadeh <moein.zargarzadeh@gmail.com>
2025-04-24 16:04:32 +0000

    Apply 1 suggestion(s) to 1 file(s)
    Co-authored-by: Hadi Fazelinia <hadifazelinia78@gmail.com>

--- src/configs/RoseNetNodeConfig.ts
+++ src/configs/RoseNetNodeConfig.ts
@@ -3,10 +3,10 @@ import { getConfigIntKeyOrDefault } from './Configs';
 
 class RoseNetNodeConfig {
   static relays = config.get<string[]>('p2p.relays');
-  static peerIdFilePath: string = config.get<string>('p2p.peerIdFilePath');
-  static host: string = config.get<string>('p2p.host');
-  static port: number = config.get<number>('p2p.port');
-  static apiCallbackTimeout: number = getConfigIntKeyOrDefault(
+  static peerIdFilePath = config.get<string>('p2p.peerIdFilePath');
+  static host = config.get<string>('p2p.host');
+  static port = config.get<number>('p2p.port');
+  static apiCallbackTimeout = getConfigIntKeyOrDefault(
     'p2p.apiCallbackTimeout',
     8
   ); // seconds
