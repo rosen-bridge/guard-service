c21dc944c3db38fd09b548a7ad7a32f1e5e3d2e0
Aco Šmrkas <ergexplorer@gmail.com>
2026-02-22 10:49:29 +0700

    removed partialHandshakeNetwork

--- packages/chains/handshake/lib/index.ts
+++ packages/chains/handshake/lib/index.ts
@@ -1,7 +1,6 @@
 export { default as HandshakeChain } from './handshakeChain';
 export { default as HandshakeTransaction } from './handshakeTransaction';
 export { default as AbstractHandshakeNetwork } from './network/abstractHandshakeNetwork';
-export { default as PartialHandshakeNetwork } from './network/partialHandshakeNetwork';
 export { default as Serializer } from './serializer';
 export * from './types';
 export * from './constants';
