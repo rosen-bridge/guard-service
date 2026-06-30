5e98777142f17c1dbabf3274b61b5759fbada1a4
arsalanyavari <arya48.yavari79@gmail.com>
2026-06-30 18:56:09 +0330

    add serviceName, format, createSymlink and symlinkName for config log file format

---
+++ .changeset/hungry-knives-mate.md
@@ -0,0 +1,6 @@
+---
+'guard-service': minor
+---
+
+add format, serviceName, createSymlink and symlinkName options for log in config, file type
+update the winston-logger from 3.0.1 to 3.1.0
--- services/guard-service/src/configs/configs.ts
+++ services/guard-service/src/configs/configs.ts
@@ -265,7 +265,11 @@ class Configs {
           log.maxSize != undefined &&
           typeof log.maxSize === 'string' &&
           log.maxFiles != undefined &&
-          typeof log.maxFiles === 'string';
+          typeof log.maxFiles === 'string' &&
+          (log.format ? typeof log.format === 'string' : true) &&
+          (log.createSymlink ? typeof log.createSymlink === 'boolean' : true) &&
+          (log.symlinkName ? typeof log.symlinkName === 'string' : true) &&
+          (log.serviceName ? typeof log.serviceName === 'string' : true);
       }
       return !(loggerChecks && logTypeValidation);
     });
